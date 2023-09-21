7a0a73e34f45c773f8aca30c2816ad9f1babd175
Maxmerock123 <Maxmerock@gmail.com>
2023-09-21 16:37:46 +0700

    add more essential function
    add game() with loop inside for play and calculate winner
    fix logic bug while adding score to player and computer
    add alert winner of each round

--- script.js
+++ script.js
@@ -28,11 +28,29 @@ function compareSelection (playerSelection, computerSelection) {
     print("winner is ...")
     return winner
 }
+
+function game(n) {
+    loop n times {
+        score = 0
+        if (player win computer){
+            score++
+        }
+        if score > 3 {
+            alert("you are ultimate winner.")
+        } else {
+            alert("you are ultimate loser.")
+        }
+    }
+}
 */
+let score = 0;
+let computerScore = 0;
+let times = 5;
 
 gameChoices = ["rock","paper","scissors"]
 function main(){
-    getPlayerChoices();
+    game(times)
+
 }
 
 function getComputerChoice() {
@@ -42,9 +60,72 @@ function getComputerChoice() {
 }
 
 function getPlayerChoices() {
-    return 0;
+   let choice = ""
+   
+   while (choice != "rock" || choice != "paper" || choice != "scissors"){
+        choice = prompt("Choose your choices(rock,paper,scissors): ").toLowerCase();
+        if (choice == "rock" || choice == "paper" || choice == "scissors"){
+            return choice
+        } else {
+            alert("wrong input")
+        }
+        
+   }
+    
 }
 
+function compareSelection(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection){
+        times++;
+        return "tie!"
+    }
+    
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore++
+        return "You Lose! paper beat rock"
+    } 
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        score++
+        return "You Win! rock beat scissors"
+    } 
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        score++
+        return "You Win! paper beat rock"
+    } 
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore++
+        return "You Lose! scissors beat paper"
+    } 
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore++
+        return "You Lose! rock beat scissors"
+    } 
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        score++
+        return "You Win! scissors beat paper"
+    } 
+
+
+}
+
+function game(){
+    let i = 0;
+    while (i != times){
+        const playerSelection = getPlayerChoices();
+        const computerSelection = getComputerChoice();
+        alert(compareSelection(playerSelection, computerSelection))
+        console.log("times: ",times)
+        i++
+        if (score == 3){
+            alert("You reached 3 score. You are ultimate winner!")
+            break
+        }else if (computerScore == 3){
+            alert("Computer reached 3 score before you, you lose. play again if you want.")
+            break
+        }
+    }
+    }
+    
 
 
 main();
